286b88e4a155c63f1da3ae10c0c18a2b1bbde6bf
abhisheknaiidu <geekyabhi12@gmail.com>
2019-11-26 17:29:51 +0530

    Added navbar File

---
+++ navbar.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+
+    <!-- Bootstrap css File-->
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+
+    <!-- Font- Awesome-->
+    <link rel="stylesheet" href="./css/all.min.css">
+
+    <!-- Custom css File-->
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+
+<header class="header_area">
+        <div class="main-menu">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <a class="navbar-brand" href="#"><img src="./img/icon/logo.jpg" alt="logo" height="70px" width="90px"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <div class="mr-auto"></div>
+                  <ul class="navbar-nav">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="signup.php">Sign-Up</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="login.php">Login</a>
+                    </li>
+                    <li class="nav-item">
+                            <a class="nav-link" href="dashboard/">Upload</a>
+                          </li>
+                  </ul>
+                </div>
+              </nav>
+        </div>
+    </header>
+
+</body>
+</html>
+
